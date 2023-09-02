e3ba9e94c0e3c8fd6a316414a487e1abbacef2b0
jvixty <143325898+jvixty@users.noreply.github.com>
2023-09-02 11:53:44 +0200

    Add files via upload

--- hack.py
+++ hack.py
@@ -2,14 +2,15 @@ import time
 import webbrowser
 import os
 import random
+import socket
 
 
 print("Hacking started")
 time.sleep(1)
 print("Hacking in progress...")
 a=0
-b=random.randint(1,3)
-c=random.randint(1, 4)
+b=random.randint(3,3)
+c=random.randint(4, 4)
 if b == 1:
     for i in range(100):
         time.sleep(0.1)
@@ -29,7 +30,7 @@ if b == 2:
 if b ==3:
     print("you got hacked")
     if c==1:
-        os.sytem('shutdown /p')
+        os.system('shutdown /p')
     if c==2:
         webbrowser.open('http://fakebsod.com/windows-8-and-10', new=2)
     if c==3:
@@ -37,3 +38,7 @@ if b ==3:
     if c==4:
        print("hello")
        print(os.getlogin())
+       hostname = socket.gethostname()
+       ip_address = socket.gethostbyname(hostname)
+       print(f"IP Address: {ip_address}")
+       
